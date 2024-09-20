dcdbc8afdd40b1cc6d4b9060caa19e9ad5441c33
Marcos <marcoscolombo66@gmail.com>
2024-09-20 14:08:41 -0300

    del images

--- images/fascination/application/config/facebook.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['appId'] = 'tu appId';
-$config['secret'] = 'tu appsecret';
-/*fin del archivo de configuracion facebook.php
-*/
